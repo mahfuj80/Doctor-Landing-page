5d0dbc49dfcb34b95233faa87156416922d35eb2
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-08-15 03:01:49 +0600

    Nuxt ui and tailwind css installed

---
+++ app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  ui: {
+    colorMode: {
+      preference: "light",
+    },
+    button: {
+      default: {
+        size: "md",
+        color: "gray",
+        variant: "ghost",
+      },
+    },
+  },
+});
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  css: ["@/assets/main.css"],
+  app: {
+    head: {
+      link: [],
+    },
+  },
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui", "@nuxt/image", "@nuxtjs/google-fonts"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  colorMode: {
+    preference: "light",
+  },
+
+  googleFonts: {
+    families: {
+      Lato: true,
+    },
+    display: "swap", // Optional: This is a good practice to improve performance
+  },
+});
