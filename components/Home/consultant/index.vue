740f9916f6142bf3a319e3d29133d4f1542f15c2
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-08-16 00:26:55 +0600

    updated consultant for responsive

--- components/Home/consultant/index.vue
+++ components/Home/consultant/index.vue
@@ -2,14 +2,14 @@
 
 <template>
   <div
-    class="relative min-h-[538px] flex items-center bg-[url('/public/consultant/slider-bg-1.jpg')] bg-cover w-full h-full bg-no-repeat"
+    class="relative md:min-h-[538px] flex items-center bg-[url('/public/consultant/slider-bg-1.jpg')] bg-cover w-full h-full bg-no-repeat py-12"
   >
     <div class="absolute w-full h-full bg-[#13232F] opacity-90"></div>
     <div
       class="container mx-auto flex flex-col justify-center items-center z-10"
     >
       <!-- 1st half -->
-      <div class="flex flex-col gap-8 items-center justify-center">
+      <div class="flex flex-col gap-8 items-center justify-center px-4 md:px-0">
         <p class="font-bold text-gray-500">
           Stay healthy & strong to enjoy life
         </p>
