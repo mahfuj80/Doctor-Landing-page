2864629213a86a309e7ecc5f5542b2e52785fc54
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-08-15 17:58:07 +0600

    Section titile common component added

---
+++ components/common/title/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+// Define the props
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+  style: {
+    type: Boolean,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <p>{{ title }}</p>
+    <p>{{ subtitle }}</p>
+    <img src="/public/section-title/section-title-line.png" alt="Img" />
+  </div>
+</template>
