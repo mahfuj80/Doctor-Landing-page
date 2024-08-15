66bdb10d8b7ef0aae03ef6ef03ce19b02c2788a7
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-08-15 19:54:06 +0600

    Doctor / Team member added

--- components/common/title/index.vue
+++ components/common/title/index.vue
@@ -19,12 +19,12 @@ const props = defineProps({
 </script>
 
 <template>
-  <div :class="['md:w-1/2', isCenter ? 'w-fit mx-auto' : '']">
+  <div :class="['md:w-[80%]', isCenter ? 'w-fit mx-auto md:w-1/2' : '']">
     <div :class="['flex ', isCenter ? 'items-center justify-center' : '']">
       <div
         :class="[
           'bg-[url(/public/section-title/section-title-bg.png)] bg-contain bg-no-repeat pt-10',
-          isCenter ? 'bg-center' : '',
+          isCenter ? 'bg-center' : 'md:bg-left bg-center',
         ]"
       >
         <p
@@ -46,7 +46,10 @@ const props = defineProps({
       </div>
     </div>
     <img
-      :class="['mt-4 md:mt-8', isCenter ? 'w-fit mx-auto' : '']"
+      :class="[
+        'mt-4 md:mt-8',
+        isCenter ? 'w-fit mx-auto' : 'w-fit mx-auto md:ml-0',
+      ]"
       src="/public/section-title/section-title-line.png"
       alt="Img"
     />
--- components/team/index.vue
+++ components/team/index.vue
@@ -1,10 +1,77 @@
-<script setup lang="ts">
-  
+<script setup lang="js">
+const teamMembers = [
+  {
+    id: 1,
+    image: '/team/member1.png',
+    name: 'Rosalina D. Williamson',
+    specialization: 'Founder'
+  },
+  {
+    id: 2,
+    image: '/team/member2.png',
+    name: 'Diconda PIran Will',
+    specialization: 'dentist'
+  },
+  {
+    id: 3,
+    image: '/team/member3.png',
+    name: 'Hulk M. Kenbon',
+    specialization: 'neurologist'
+  },
+  {
+    id: 4,
+    image: '/team/member4.png',
+    name: 'Haliam Z. Dicolaz',
+    specialization: 'Consultant'
+  },
+  {
+    id: 5,
+    image: '/team/member5.png',
+    name: 'Nicolas D. Case',
+    specialization: 'dentist'
+  },
+  {
+    id: 6,
+    image: '/team/member6.png',
+    name: 'Phumdon H. Norman',
+    specialization: 'neurologist'
+  }
+];
 </script>
-  
+
 <template>
-  
-   <h1>This is index Page</h1>
-  
+  <section class="container mx-auto py-12 md:py-24">
+    <div
+      class="flex flex-col md:flex-row gap-8 items-center justify-center md:justify-between md:w-[90%] md:mx-auto"
+    >
+      <div>
+        <CommonTitle
+          title="Our Team"
+          subtitle="A Professional & Care Provider"
+          :isCenter="false"
+        />
+      </div>
+      <div><CommonButton text="Make Appointment" /></div>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 py-12 gap-12">
+      <div
+        class="flex flex-col items-center justify-center gap-4"
+        v-for="item in teamMembers"
+        :key="item.id"
+      >
+        <div class="relative">
+          <img :src="item.image" alt="Image" />
+          <UIcon
+            class="absolute right-0 bottom-10 font-bold text-white p-4 rounded-full bg-[#8FB569] hover:bg-[#E1247A] transition duration-300 text-6xl cursor-pointer"
+            name="ic:sharp-plus"
+            dynamic
+          />
+        </div>
+        <p class="text-2xl font-bold text-gray-900">{{ item.name }}</p>
+        <p class="text-md font-bold text-[#E1247A] uppercase">
+          {{ item.specialization }}
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
-  
